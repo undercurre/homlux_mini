64f6f3880fc58f3331feecb5d4bf48252a971e74
linhy29 <linhy29@midea.com>
2023-02-07 14:19:41 +0800

    feat: 去除mitt依赖，完善项目规范
    业务相关代码提取到/src/util/service.ts

--- src/store/others.ts
+++ src/store/others.ts
@@ -1,26 +1,9 @@
-import { observable, runInAction } from 'mobx-miniprogram'
-import { storage } from '../utils/index'
+import { observable } from 'mobx-miniprogram'
 
-export const others = observable({
-  token: '',
-  setToken: function (token: string) {
-    runInAction(() => {
-      others.token = token
-    })
-  },
-  logout() {
-    runInAction(() => {
-      others.token = ''
-    })
-    storage.remove('token')
-    wx.redirectTo({
-      url: '/pages/login/index',
-    })
-  },
-})
+export const others = observable({})
 
 export const othersBinding = {
   store: others,
-  fields: ['token'],
-  actions: ['setToken', 'logout'],
+  fields: [],
+  actions: [],
 }
