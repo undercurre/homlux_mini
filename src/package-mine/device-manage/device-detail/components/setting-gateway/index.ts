488cc3c178e6689909ab1e7214c2b758763a54c2
linhy29 <linhy29@midea.com>
2023-03-03 09:02:09 +0800

    feat: 完善网关管理页界面

--- src/package-mine/device-manage/device-detail/components/setting-gateway/index.ts
+++ src/package-mine/device-manage/device-detail/components/setting-gateway/index.ts
@@ -6,15 +6,43 @@ Component({
   /**
    * 组件的属性列表
    */
-  properties: {},
+  properties: {
+    deviceInfo: {
+      type: Object,
+    },
+  },
 
   /**
    * 组件的初始数据
    */
-  data: {},
+  data: {
+    backup: false,
+    resetting: false,
+  },
 
   /**
    * 组件的方法列表
    */
-  methods: {},
+  methods: {
+    handleBackup() {
+      this.setData({
+        backup: true,
+      })
+      setTimeout(() => {
+        this.setData({
+          backup: false,
+        })
+      }, 3000)
+    },
+    handleReset() {
+      this.setData({
+        resetting: true,
+      })
+      setTimeout(() => {
+        this.setData({
+          resetting: false,
+        })
+      }, 3000)
+    },
+  },
 })
