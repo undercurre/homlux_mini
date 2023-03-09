dd75b73a0bd6bbbdd9e559b8fa7fffe74f4b0435
冼永恒 <xianyh1@midea.com>
2023-03-09 13:42:53 +0800

    fix: 页面UI样式修复

--- src/assets/svg/index.ts
+++ src/assets/svg/index.ts
@@ -5,5 +5,6 @@ export default {
   ...iconfont,
   ...iconify,
   ...scene,
-  'room-bg': '<svg xmlns="http://www.w3.org/2000/svg" width="375" height="328"><defs><linearGradient id="a" x1="50%" x2="50%" y1="8.747%" y2="72.963%"><stop offset="0%" stop-color="#5769FF" stop-opacity=".104"/><stop offset="100%" stop-color="#90A2FF" stop-opacity="0"/></linearGradient></defs><path fill="url(#a)" fill-rule="evenodd" d="M0 0h376v450H0z"/></svg>',
+  'room-bg':
+    '<svg xmlns="http://www.w3.org/2000/svg" width="375" height="328"><defs><linearGradient id="a" x1="50%" x2="50%" y1="8.747%" y2="72.963%"><stop offset="0%" stop-color="#5769FF" stop-opacity=".104"/><stop offset="100%" stop-color="#90A2FF" stop-opacity="0"/></linearGradient></defs><path fill="url(#a)" fill-rule="evenodd" d="M0 0h376v450H0z"/></svg>',
 }
