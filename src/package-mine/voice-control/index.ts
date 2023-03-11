96b92af06714ab4d125614c9dbad8ae7875e6293
KEVIN YANG <yangws2@midea.com>
2023-03-11 17:13:37 +0800

    feat(projects): 小度语音链接页初步提交

---
+++ src/package-mine/voice-control/index.ts
@@ -0,0 +1,37 @@
+// package-mine/hoom-manage/index.ts
+import { BehaviorWithStore } from 'mobx-miniprogram-bindings'
+import { ComponentWithComputed } from 'miniprogram-computed'
+import pageBehaviors from '../../behaviors/pageBehaviors'
+import { homeBinding, userBinding } from '../../store/index'
+
+ComponentWithComputed({
+  options: {
+    addGlobalClass: true,
+  },
+  behaviors: [BehaviorWithStore({ storeBindings: [homeBinding, userBinding] }), pageBehaviors],
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  computed: {},
+
+  lifetimes: {
+    // 生命周期函数，可以为函数，或一个在 methods 段中定义的方法名
+    attached: function () {
+
+    },
+    moved: function () {},
+    detached: function () {},
+  },
+
+  methods: {
+
+
+
+
+  },
+})
