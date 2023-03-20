48378878af7629dac75b5dc7dea745732eb715fd
linhy29 <linhy29@midea.com>
2023-03-20 20:23:28 +0800

    fix: 我的界面调整、开关选择添加动画

--- src/package-room-control/components/scene-switch-edit-popup/index.ts
+++ src/package-room-control/components/scene-switch-edit-popup/index.ts
@@ -17,6 +17,7 @@ ComponentWithComputed({
         if (value) {
           this.setData({
             OnOff: this.data.switchInfo.OnOff,
+            top: this.data.switchInfo.OnOff ? 0 : 120,
           })
         }
       },
@@ -31,6 +32,7 @@ ComponentWithComputed({
    */
   data: {
     OnOff: 0,
+    top: 0,
   },
 
   /**
@@ -44,9 +46,24 @@ ComponentWithComputed({
       this.triggerEvent('confirm', { OnOff: this.data.OnOff })
     },
     handleOnOffChange(e: WechatMiniprogram.TouchEvent) {
+      if (e.currentTarget.dataset.value === this.data.OnOff) {
+        return
+      }
       this.setData({
         OnOff: e.currentTarget.dataset.value,
       })
+      this.animate(
+        '#slider',
+        [
+          {
+            top: this.data.OnOff ? '120rpx' : '0',
+          },
+          {
+            top: this.data.OnOff ? '0' : '120rpx',
+          },
+        ],
+        100,
+      )
     },
   },
 })
