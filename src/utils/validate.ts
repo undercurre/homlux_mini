da4338b6b4bc46bc5e35c98e8af86f614a475ca0
linhy29 <linhy29@midea.com>
2023-03-29 14:28:39 +0800

    fix: 限制设备、按键名字数为5个，场景名为4个

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -2,5 +2,5 @@
  * 校验输入的是否只包含中文、英文、数字、小数点、英文中/下划线
  */
 export function validateInputName(input: string) {
-  return !/[^/a-zA-Z0-9\u4E00-\u9FA5]/g.test(input)
+  return !/[^a-zA-Z0-9\u4E00-\u9FA5]/g.test(input)
 }
