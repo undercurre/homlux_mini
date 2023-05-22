48415bd115bce3b9a4d0c72ad618557f0aba99d0
KEVIN YANG <yangws2@midea.com>
2023-05-22 15:30:27 +0800

    fix: 修正点击面板卡片时报错的问题

--- src/package-room-control/index/components/device-control-popup/index.ts
+++ src/package-room-control/index/components/device-control-popup/index.ts
@@ -55,6 +55,9 @@ ComponentWithComputed({
         // 色温范围计算
         else if (value.length) {
           const deviceId = this.data.checkedList[0]
+          if (deviceId.indexOf(':') !== -1) {
+            return // 排除面板
+          }
           const deviceMap = deviceStore.allRoomDeviceMap
           const { productId } = deviceMap[deviceId]
           const [minColorTempK, maxColorTempK] = colorTempKRange[productId]
