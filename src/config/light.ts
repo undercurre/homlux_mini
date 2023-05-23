b8d75df7cc6040f64144fd91cbd0b32eef5cc7f2
KEVIN YANG <yangws2@midea.com>
2023-05-23 17:01:12 +0800

    fix: 补充睿铂筒灯色温配置

--- src/config/light.ts
+++ src/config/light.ts
@@ -4,6 +4,7 @@ export const minColorTempK = 2400
 
 // 根据 productId 判断色温
 export const colorTempKRange: Record<string, number[]> = {
-  'midea.hlight.001.001': [2700, 6500],
-  'midea.hlight.002.001': [2400, 6500],
+  'midea.hlight.001.001': [2700, 6500], // 睿逸筒灯
+  'midea.hlight.002.001': [2400, 6500], // 灯带
+  'midea.hlight.003.001': [2700, 6500], // 睿铂筒灯
 }
