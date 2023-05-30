86a38e366eb86e82c47db7caa482424d0d635256
KEVIN YANG <yangws2@midea.com>
2023-05-30 16:09:38 +0800

    feat: 修改首页亮灯数统计逻辑

--- src/store/room.ts
+++ src/store/room.ts
@@ -51,9 +51,9 @@ export const roomStore = observable({
         } else if (device.proType === proType.switch) {
           device.switchInfoDTOList.forEach((switchItem) => {
             if (
-              !switchItem.lightRelId &&
+              // !switchItem.lightRelId &&
               device.mzgdPropertyDTOList[switchItem.switchId].OnOff &&
-              !device.mzgdPropertyDTOList[switchItem.switchId].ButtonMode
+              device.mzgdPropertyDTOList[switchItem.switchId].ButtonMode === 0
             ) {
               deviceLightOnNum++
             }
