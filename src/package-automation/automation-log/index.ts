e5b1e400e6e013d7cecbb65b37d03ea7a3bac27e
夏钰明 <xiaym18@midea.com>
2023-06-02 15:48:23 +0800

    feat: 自动化日志页面UI开发

---
+++ src/package-automation/automation-log/index.ts
@@ -0,0 +1,60 @@
+import pageBehavior from '../../behaviors/pageBehaviors'
+// import { homeBinding, homeStore, otaBinding, otaStore } from '../../store/index'
+// import Toast from '@vant/weapp/toast/toast'
+import { ComponentWithComputed } from 'miniprogram-computed'
+// import { BehaviorWithStore } from 'mobx-miniprogram-bindings'
+// import { getEnv } from '../../config/index'
+ComponentWithComputed({
+  behaviors: [pageBehavior],
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    autoUpdate: false,
+    isLoading: false,
+    contentHeight: 0,
+    autoSceneLog: {
+      '6月23日': [
+        { id: 0, name: '早上起床模式', desc: '早上7:00', icon: 'all-on', isSuccess: '已执行' },
+        { id: 1, name: '早上起床模式', desc: '早上7:00', icon: 'all-on', isSuccess: '执行失败' },
+      ],
+      '5月30日': [
+        { id: 0, name: '客厅无人关闭模式客...', desc: '上午12:00', icon: 'mild', isSuccess: '已执行' },
+        { id: 1, name: '客厅无人关闭模式', desc: '上午12:00', icon: 'mild', isSuccess: '执行失败' },
+      ],
+      // '4月30日': [
+      //   { id: 0, name: '客厅无人关闭模式客...', desc: '上午12:00', icon: 'mild', isSuccess: '已执行' },
+      //   { id: 1, name: '客厅无人关闭模式', desc: '上午12:00', icon: 'mild', isSuccess: '执行失败' },
+      // ],
+    },
+    isUpdating: false,
+    hasUpdate: false,
+    fromDevice: false,
+    _pollingTimer: 0,
+  },
+
+  computed: {
+    // canOTA(data) {
+    //   return data.isCreator || data.isAdmin
+    // },
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    onLoad() {
+      wx.createSelectorQuery()
+        .select('#content')
+        .boundingClientRect()
+        .exec((res) => {
+          if (res[0] && res[0].height) {
+            this.setData({
+              contentHeight: res[0].height,
+            })
+          }
+        })
+    },
+  },
+})
--- src/pages/automation/index.ts
+++ src/pages/automation/index.ts
@@ -20,12 +20,19 @@ Component({
       { name: '早上起床模式', desc: '每天', icon: 'all-on', onoff: true },
       { name: '早上起床模式', desc: '每天', icon: 'all-on', onoff: true },
       { name: '早上起床模式', desc: '每天', icon: 'all-on', onoff: true },
-      { name: '早上起床模式', desc: '每天', icon: 'all-on', onoff: true },
+      { name: '客厅无人关闭模式客...', desc: '每天', icon: 'all-on', onoff: true },
       { name: '早上起床模式', desc: '每天', icon: 'all-on', onoff: true },
       { name: '客厅无人关闭模式', desc: '每天', icon: 'mild', onoff: false },
     ],
-    // checkImg: '/assets/img/base/check.png',
-    // uncheckImg: '/assets/img/base/uncheck.png',
+    urls: {
+      automationLog: '/package-automation/automation-log/index',
+      voiceControl: '/package-mine/voice-control/index',
+      ota: '/package-mine/ota/index',
+      deviceReplace: '/package-mine/device-replace/index',
+      auth: '/package-mine/auth/index',
+      feedback: '/package-mine/feedback/index',
+      about: '/package-protocol/protocol-list/index',
+    },
     // 导航栏和状态栏高度
     navigationBarAndStatusBarHeight:
       (storage.get<number>('statusBarHeight') as number) +
@@ -41,6 +48,12 @@ Component({
           selected: 1,
         })
       }
+    },
+    toPage(e: { currentTarget: { dataset: { url: string } } }) {
+      console.log('111', e.currentTarget.dataset.url)
+      wx.navigateTo({
+        url: e.currentTarget.dataset.url,
+      })
     },
   },
 })
