4dd3f49a74edda7461800f6bebbc6a4ed71c2e02
夏钰明 <xiaym18@midea.com>
2023-06-15 16:28:15 +0800

    feat: 开发CurtainSlider窗帘组件

---
+++ src/components/curtain-slider/index.ts
@@ -0,0 +1,49 @@
+Component({
+  externalClasses: ['custom-class'],
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    // disabled: Boolean,
+
+    max: {
+      type: Number,
+      value: 100,
+    },
+    min: {
+      type: Number,
+      value: 0,
+    },
+    step: {
+      type: Number,
+      value: 1,
+    },
+    value: {
+      type: Number,
+      value: 0,
+    },
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {},
+
+  lifetimes: {
+    attached() {},
+    ready() {},
+    detached() {},
+  },
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    valueChange(e: { value: number }) {
+      this.triggerEvent('change', e.value)
+      this.setData({
+        value: e.value,
+      })
+      console.log(this.data.value)
+    },
+  },
+})
