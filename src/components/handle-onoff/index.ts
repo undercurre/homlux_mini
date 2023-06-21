8048e2fd83e880cd9398aa7bca6f45a02b53016a
夏钰明 <xiaym18@midea.com>
2023-06-21 11:25:42 +0800

    feat: 改造handle-onoff组件，解决原有缺陷

--- src/components/handle-onoff/index.ts
+++ src/components/handle-onoff/index.ts
@@ -1,15 +1,12 @@
-import { ComponentWithComputed } from 'miniprogram-computed'
+import { getRect } from '../../utils/index'
 
-ComponentWithComputed({
+Component({
   externalClasses: ['custom-class'],
   /**
    * 组件的属性列表
    */
   properties: {
     // disabled: Boolean,
-    show: {
-      type: Boolean,
-    },
     OnOff: {
       type: Boolean,
       value: false,
@@ -23,29 +20,9 @@ ComponentWithComputed({
   /**
    * 组件的初始数据
    */
-  data: {
-    sliderWidth: 0,
-  },
-  computed: {
-    isShow(data: { show: boolean; vertical: boolean }) {
-      return { show: data.show, vertical: data.vertical }
-    },
-  },
+  data: {},
 
-  observers: {
-    show: function () {
-      setTimeout(() => {
-        this.createSelectorQuery()
-          .select('#slider')
-          .boundingClientRect((res) => {
-            this.setData({
-              sliderWidth: res.width,
-            })
-          })
-          .exec()
-      }, 300)
-    },
-  },
+  observers: {},
   lifetimes: {
     attached() {},
     ready() {},
@@ -55,41 +32,52 @@ ComponentWithComputed({
    * 组件的方法列表
    */
   methods: {
-    handleOnOffChange(e: WechatMiniprogram.TouchEvent) {
+    async handleOnOffChange(e: WechatMiniprogram.TouchEvent) {
       if (e.currentTarget.dataset.value === this.data.OnOff) {
         return
       }
-
-      this.setData({
-        OnOff: e.currentTarget.dataset.value,
-      })
-      this.triggerEvent('OnOffChange', this.data.OnOff)
+      const { width } = await getRect(this, '#slider')
 
       if (this.data.vertical) {
         this.animate(
           '#slider',
           [
             {
-              top: this.data.OnOff ? '96rpx' : '0',
+              top: e.currentTarget.dataset.value ? '96rpx' : '0',
             },
             {
-              top: this.data.OnOff ? '0' : '96rpx',
+              top: e.currentTarget.dataset.value ? '0' : '96rpx',
             },
           ],
           100,
+          () => {
+            //先设置原style，再清除动画样式避免样式造成闪烁问题
+            this.setData({
+              OnOff: e.currentTarget.dataset.value,
+            })
+            this.triggerEvent('change', this.data.OnOff)
+            this.clearAnimation('#slider', () => {})
+          },
         )
       } else {
         this.animate(
           '#slider',
           [
             {
-              left: this.data.OnOff ? this.data.sliderWidth + 'px' : '0',
+              left: e.currentTarget.dataset.value ? width + 'px' : '0',
             },
             {
-              left: this.data.OnOff ? '0' : this.data.sliderWidth + 'px',
+              left: e.currentTarget.dataset.value ? '0' : width + 'px',
             },
           ],
           100,
+          () => {
+            this.setData({
+              OnOff: e.currentTarget.dataset.value,
+            })
+            this.triggerEvent('change', this.data.OnOff)
+            this.clearAnimation('#slider', () => {})
+          },
         )
       }
     },
