6282782008a42beedaebca2c9961c0905c24c72f
夏钰明 <xiaym18@midea.com>
2023-07-19 10:26:09 +0800

    feat: 自动化列表增加ws监听更新

--- src/pages/automation/index.ts
+++ src/pages/automation/index.ts
@@ -6,6 +6,7 @@ import { ComponentWithComputed } from 'miniprogram-computed'
 import { BehaviorWithStore } from 'mobx-miniprogram-bindings'
 import { strUtil } from '../../utils/strUtil'
 import { reaction } from 'mobx-miniprogram'
+import { emitter } from '../../utils/index'
 
 // pages/login/index.ts
 ComponentWithComputed({
@@ -29,7 +30,7 @@ ComponentWithComputed({
   },
   computed: {},
   methods: {
-    async onLoad() {
+    onLoad() {
       // 更新tabbar状态
       if (typeof this.getTabBar === 'function' && this.getTabBar()) {
         this.getTabBar().setData({
@@ -43,10 +44,19 @@ ComponentWithComputed({
           autosceneBinding.store.updateAllRoomAutoSceneList()
         },
       )
+      emitter.on('wsReceive', (res) => {
+        const { eventType } = res.result
+        if (eventType === 'scene_add' || eventType === 'scene_del' || eventType === 'scene_upt') {
+          autosceneBinding.store.updateAllRoomAutoSceneList()
+        }
+      })
       //加载自动化列表
       if (this.data.isLogin) {
         autosceneBinding.store.updateAllRoomAutoSceneList()
       }
+    },
+    onUnload() {
+      emitter.off('wsReceive')
     },
     toPage(e: { currentTarget: { dataset: { url: string } } }) {
       wx.navigateTo({
