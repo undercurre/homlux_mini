b77681bda58d16531adc332547624ef7dcc8d24a
xianyh1 <xianyh1@midea.com>
2023-07-27 13:44:26 +0800

    feat: 蓝牙子设备配网，增加panId、exPanId的下发

--- src/utils/bleProtocol.ts
+++ src/utils/bleProtocol.ts
@@ -281,7 +281,9 @@ export class BleClient {
 
   async startZigbeeNet({ channel = 0, panId = 0, extPanId = 0 }) {
     const panIdHexArr = strUtil.hexStringToArrayUnit8(panId.toString(16).toUpperCase().padStart(4, '0'), 2).reverse()
-    const exPanIdHexArr = strUtil.hexStringToArrayUnit8(extPanId.toString(16).toUpperCase().padStart(16, '0'), 2).reverse()
+    const exPanIdHexArr = strUtil
+      .hexStringToArrayUnit8(extPanId.toString(16).toUpperCase().padStart(16, '0'), 2)
+      .reverse()
 
     console.debug('panIdHexArr', panIdHexArr, 'exPanIdHexArr', exPanIdHexArr)
 
