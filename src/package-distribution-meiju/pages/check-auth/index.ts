dc730b22e9af71f8425d36442473230e00de322d
xianyh1 <xianyh1@midea.com>
2023-11-22 10:51:13 +0800

    feat: 美居浴霸配网指引页面特殊处理

--- src/package-distribution-meiju/pages/check-auth/index.ts
+++ src/package-distribution-meiju/pages/check-auth/index.ts
@@ -8,6 +8,7 @@ import { addDeviceSDK } from '../../utils/addDeviceSDK'
 import { addGuide, inputWifiInfo } from '../../utils/paths.js'
 import Toast from '@vant/weapp/toast/toast'
 import Dialog from '@vant/weapp/dialog/dialog'
+import { meijuImgDir } from '../../../config/img'
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { getLinkType } = require('../assets/js/utils.js')
 
@@ -126,20 +127,37 @@ ComponentWithComputed({
       }
 
       const guideInfo = res.result
+      const connectDesc = guideInfo.mainConnectTypeDesc
+      let connectUrl = guideInfo.mainConnectTypeUrlList[0]
+      const guideInfoList = []
+
+      // 若品类为浴霸，写死附加型号为R1的配网方式，并固定配网图
+      if (type === '26') {
+        guideInfoList.push({
+          connectDesc:
+            '① 浴霸接通电源\n② 检查遥控器是否能够控制浴霸(如：按遥控器「照明」键，浴霸灯亮)\n③ 长按遥控器「+」键「5」秒，听到“嘀”提示音，数秒内WiFi指示灯闪烁，表示设置成功',
+          connectUrlA: 'http://midea-file.oss-cn-hangzhou.aliyuncs.com/2021/5/31/9/vLzXTxNBLxzGKAErIpYf.gif',
+          isAutoConnect: guideInfo.isAutoConnect,
+          code: guideInfo.modelCode,
+          wifiFrequencyBand: guideInfo.wifiFrequencyBand,
+        })
+
+        connectUrl = `${meijuImgDir}/addDevice/bath-heater-guide.gif`
+      }
+
+      guideInfoList.unshift({
+        connectDesc,
+        connectUrlA: connectUrl,
+        isAutoConnect: guideInfo.isAutoConnect,
+        code: guideInfo.modelCode,
+        wifiFrequencyBand: guideInfo.wifiFrequencyBand,
+      })
       //0,3 跳inputWifiInfo, 5 跳addguide
       const addDeviceInfo = {
         enterprise: '0000',
         fm: 'selectType',
         linkType: getLinkType(mode),
-        guideInfo: [
-          {
-            connectDesc: guideInfo.mainConnectTypeDesc,
-            connectUrlA: guideInfo.mainConnectTypeUrlList[0],
-            isAutoConnect: guideInfo.isAutoConnect,
-            code: guideInfo.modelCode,
-            wifiFrequencyBand: guideInfo.wifiFrequencyBand,
-          },
-        ],
+        guideInfo: guideInfoList,
       }
       const modeArr = addDeviceSDK.supportAddDeviceMode
 
