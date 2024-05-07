c2eb2cbc9f0aab737489b9935eb9e396445832fb
夏钰明 <xiaym18@midea.com>
2024-05-07 17:29:41 +0800

    feat: 删除mz-drag-list调试代码

--- src/skyline-components/mz-drag-list/index.ts
+++ src/skyline-components/mz-drag-list/index.ts
@@ -71,8 +71,7 @@ Component({
      */
     init() {
       const { windowHeight, platform } = wx.getSystemInfoSync()
-      console.log('sssdsdsds', this.data.topSize, this.data.bottomSize)
-
+      console.log('drag的topsize和bottomsize', this.data.topSize, this.data.bottomSize)
       Object.assign(this.data._baseData, {
         platform: platform,
         windowHeight: windowHeight,
@@ -101,8 +100,6 @@ Component({
       listData.forEach((item, index) => {
         list.push(delItem(item, index))
       })
-
-      // this.data.listWxs = list
       this.setData(
         {
           list,
@@ -110,9 +107,7 @@ Component({
         },
         () => {
           // 绑定动画
-          // this.itemStyle = Object.create({})
           this.data.list.forEach((item) => {
-            // const top = shared((100 / this.data._baseData.rows) * item.sortKey)
             const translationY = shared(0)
             const opacity = shared(1)
             this.applyAnimatedStyle(
@@ -120,19 +115,12 @@ Component({
               () => {
                 'worklet'
                 return {
-                  // top: top.value + '%',
                   transform: `translateY(${translationY.value}%)`,
-
                   opacity: opacity.value,
-                  // backgroundColor: opacity.value ? "red" : "green",
                 }
-                // this._offset = offset
               },
               { immediate: true, flush: 'async' },
             )
-            // top.value = timing((100 / this.data._baseData.rows) * item.sortKey, {
-            //   duration: 300,
-            // })
             this.data._itemStyle[item.id] = { translationY, opacity }
           })
           const tempItemTop = shared(0)
@@ -149,9 +137,6 @@ Component({
             { immediate: true, flush: 'async' },
           )
           this.data._tempItemStyle = { tempItemTop, tempItemOpacity }
-          // this.tempItemTop = tempItemTop
-          // this.tempItemOpacity = tempItemOpacity
-          console.log(this)
         },
       )
 
@@ -168,8 +153,7 @@ Component({
         if (!res || !res[0]) {
           return
         }
-        console.log('wwwwwwwwwwww', res[0])
-
+        console.log('initDom的信息', res[0])
         Object.assign(this.data._baseData, {
           itemHeight: res[0].height,
           wrapTopSize: res[0].top,
@@ -178,22 +162,6 @@ Component({
             (res[0].top - this.data._baseData.realTopSize) -
             (this.data._baseData.realBottomSize - this.data._baseData.realTopSize),
         })
-        console.log(
-          '为什么',
-          this.data._baseData.rows,
-          res[0].height,
-          res[0].top,
-          this.data._baseData.realBottomSize,
-          this.data._baseData.realTopSize,
-        )
-
-        console.log(
-          'this.data.baseData22',
-          this.data._baseData,
-          this.data._baseData.rows * res[0].height,
-          this.data._baseData.realBottomSize - this.data._baseData.realTopSize,
-        )
-
         this.setData({
           wrapStyle: `height: ${this.data._baseData.rows * this.data._baseData.itemHeight}px`,
         })
@@ -207,34 +175,9 @@ Component({
         scrollTop: e.scrollTop,
       })
     },
-    drag(e: { dragging: boolean; index: number }) {
-      console.log('触发drag', this.data.dragging)
-      this.triggerEvent('drag', {
-        dragging: e.dragging,
-        ...this.data.listData[e.index],
-      })
-      this.setData({
-        dragging: e.dragging,
-      })
-    },
-    //TODO
-    // itemClick(e: WechatMiniprogram.TouchEvent) {
-    //   const index = e.currentTarget.dataset.index;
-    //   const item = this.data.listWxs[index];
-    //   console.log("itemClick");
-
-    //   this.triggerEvent("click", {
-    //     key: item.realKey,
-    //     data: item.data,
-    //     extra: e.detail,
-    //   });
-    // },
 
     longPress(evt: { state: number; currentTarget: { dataset: { id: string; sortkey: number } }; absoluteY: number }) {
       'worklet'
-      // console.log(evt)
-      // console.log(this);
-
       if (evt.state === 1) {
         runOnJS(this.toggleDragging.bind(this))(true, evt.currentTarget.dataset)
         runOnJS(this.setTempItemValue.bind(this))(evt.absoluteY)
@@ -247,17 +190,11 @@ Component({
     },
 
     toggleDragging(isDragging: boolean, dataset = { id: '', sortkey: 0 }) {
-      console.log('this.data.dragging', this, dataset.id, isDragging)
       this.data._scrollTopReady = false
       // 如果已经在 drag 中则禁止操作, 防止多指触发 drag 动作
       if (isDragging && this.data.dragging) return
-      this.data._itemStyle[dataset.id].opacity.value = isDragging ? 0.5 : 1
-      // let curDragItem = {data:{},id:'',sortKey:-1,tempSortKey:-1}
-      // if(isDragging){
-      //   curDragItem = this.data.list.find((item) => item.id === dataset.id)
-      // }
-      this.data._tempItemStyle.tempItemOpacity.value = isDragging ? 1 : 0.5
-      // console.log('狗屎', curDragItem);
+      this.data._itemStyle[dataset.id].opacity.value = isDragging ? 0 : 1
+      this.data._tempItemStyle.tempItemOpacity.value = isDragging ? 1 : 0
 
       this.setData(
         {
@@ -266,15 +203,12 @@ Component({
           curDragItem: isDragging ? this.data.list.find((item) => item.id === dataset.id) : this.data.curDragItem,
         },
         () => {
-          console.log('哈哈哈')
-
           if (isDragging) {
             this.vibrate()
             this.createSelectorQuery()
               .select(`#ID${dataset.id}`)
               .boundingClientRect()
               .exec((res) => {
-                console.log('res', res)
                 this.data._scrollTop =
                   this.data._baseData.realTopSize +
                   (this.data._baseData.wrapTopSize - this.data._baseData.realTopSize) +
@@ -282,7 +216,7 @@ Component({
                   res[0].top
                 this.data._scrollTopReady = true
 
-                console.log(this.data._scrollTop)
+                console.log('此时scrollTop', this.data._scrollTop)
               })
           } else {
             if (!isDragging) {
@@ -293,27 +227,21 @@ Component({
               this.triggerEvent('sortend', {
                 listData: this.data.list,
               })
-              console.log('aaaaaaaaaa', this.data._preEndKey, this.data.list)
             }
           }
         },
       )
     },
     setTempItemValue(value: number) {
-      // console.log(this.tempItemTop, value)
       this.data._tempItemStyle.tempItemTop.value = value - this.data._baseData.itemHeight / 2
-      // this.tempItemTop.value = value - this.data._baseData.itemHeight / 2
     },
     isOutRange(cY: number) {
       return cY < this.data._baseData.realTopSize || cY > this.data._baseData.realBottomSize
     },
 
     touchMove(evt: { absoluteY: number }) {
-      // console.log('111111111');
       //安卓真机上会快速触发多次手势
       if (!this.data._scrollTopReady) return
-      // console.log('22222222222');
-      // console.log("ewewew", evt, this.data.curDragItem);
       if (this.data._autoMoveTimerId === null && !this.isOutRange(evt.absoluteY)) {
         this.touchAutoMove(evt.absoluteY)
       }
@@ -334,8 +262,6 @@ Component({
         evt.absoluteY > this.data._baseData.realBottomSize &&
         this.data._scrollTop <= this.data._baseData.maxScrollTop
       ) {
-        console.log('看一下', this.data._scrollTop, this.data._baseData.maxScrollTop)
-
         if (this.data._autoMoveTimerId === null) {
           this.data._autoMoveTimerId = setInterval(() => {
             if (this.data._scrollTop >= this.data._baseData.maxScrollTop) {
@@ -365,18 +291,6 @@ Component({
         (this.data._scrollTop + (absoluteY - this.data._baseData.itemHeight / 2) - this.data._baseData.wrapTopSize) /
           this.data._baseData.itemHeight,
       )
-      // console.log(this.data.scrollTop,evt.absoluteY,this.data.topSize,this.data.itemHeight,(this.data.scrollTop + (evt.absoluteY) - this.data.topSize) / this.data.itemHeight);
-      // console.log(startKey, endKey, this.data._preEndKey, startKey === endKey);
-      // console.log(
-      //   '钱钱钱',
-      //   Math.round(
-      //     (this.data._scrollTop + (absoluteY - this.data._baseData.itemHeight / 2) - this.data._baseData.realTopSize) /
-      //       this.data._baseData.itemHeight,
-      //   ),
-      //   (this.data._scrollTop + (absoluteY - this.data._baseData.itemHeight / 2) - this.data._baseData.realTopSize) /
-      //     this.data._baseData.itemHeight,
-      // )
-      // console.log('哇哇哇', this.data._scrollTop, absoluteY, direction)
 
       // 防止拖拽过程中发生乱序问题
       if (
@@ -386,19 +300,12 @@ Component({
         endKey > this.data.list.length - 1
       )
         return
-
-      // if (startKey === endKey || endKey === this.data._preEndKey) return;
-      // if (startKey === endKey) return;
-
-      // this.data._preStartKey = startKey;
       this.data._preEndKey = endKey
 
       this.sortCore(startKey, endKey)
       this.vibrate() // 触发震动
     },
     sortCore(sKey: number, eKey: number) {
-      console.log('sortCore', this.data.list)
-
       this.data.list.forEach((item) => {
         let tKey = item.sortKey
         if (tKey === sKey) {
@@ -408,9 +315,6 @@ Component({
         } else if (sKey > eKey && tKey >= eKey && tKey < sKey) {
           ++tKey
         }
-        // if (item.tempSortKey !== rKey) {
-
-        // item.realSortKey = item.realSortKey + rKey
 
         this.data._itemStyle[item.id].translationY.value = timing(
           (tKey - item.realSortKey) * 100,
@@ -424,12 +328,10 @@ Component({
         if (item.tempSortKey !== tKey) item.tempSortKey = tKey
       })
     },
-
-    touchEnd() {
-      this.data.list.forEach((item) => {
-        item.sortKey = item.tempSortKey
-      })
-    },
+    /**
+     * 向外暴露自定义事件
+     * @param e
+     */
     onItemClick(e: { detail: IAnyObject }) {
       this.triggerEvent('itemClick', e.detail)
     },
