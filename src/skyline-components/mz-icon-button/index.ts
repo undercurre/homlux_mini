badef7b97d47795eba4d463486dce09f2dc5b96c
KEVIN YANG <yangws2@midea.com>
2024-05-15 17:40:36 +0800

    feat: 优化图标按钮

--- src/skyline-components/mz-icon-button/index.ts
+++ src/skyline-components/mz-icon-button/index.ts
@@ -1,4 +1,7 @@
-Component({
+import { ComponentWithComputed } from 'miniprogram-computed'
+import { timing, Easing } from '../common/worklet'
+
+ComponentWithComputed({
   properties: {
     disabled: Boolean,
     icon: {
@@ -8,6 +11,26 @@ Component({
     iconActive: {
       type: String,
       value: '',
+    },
+    imageSize: {
+      type: Number,
+      value: 64,
+    },
+    wrapperWidth: {
+      type: Number,
+      value: 80,
+    },
+    wrapperHeight: {
+      type: Number,
+      value: 80,
+    },
+    round: {
+      type: Number,
+      value: 48,
+    },
+    textColor: {
+      type: String,
+      value: '#555659',
     },
     bgColor: {
       type: String,
@@ -20,6 +43,10 @@ Component({
     text: {
       type: String,
       value: '',
+    },
+    isOn: {
+      type: Boolean,
+      value: false,
     },
   },
 
@@ -59,6 +86,19 @@ Component({
     },
   },
 
+  computed: {
+    imageStyle(data) {
+      const { imageSize } = data
+      return `width: ${imageSize}rpx; height: ${imageSize}rpx;
+      margin-left: -${imageSize / 2}rpx; margin-top: -${imageSize / 2}rpx;`
+    },
+    wrapperStyle(data) {
+      const { wrapperWidth, wrapperHeight, round, bgColor, bgColorActive, isOn } = data
+      const _bgColor = isOn ? bgColorActive : bgColor
+      return `width: ${wrapperWidth}rpx; height: ${wrapperHeight}rpx; border-radius: ${round}rpx; background-color: ${_bgColor};`
+    },
+  },
+
   methods: {
     handleTouchStart(e: WechatMiniprogram.TouchEvent) {
       this.triggerEvent('btnTouchStart', e.detail)
@@ -67,7 +107,18 @@ Component({
 
       this.data._bg_color.value = this.data.bgColorActive
       if (this.data.iconActive) {
-        this.data._btn_opacity.value = 0
+        this.data._btn_opacity.value = timing(
+          0,
+          {
+            duration: 150,
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            easing: Easing.ease,
+          },
+          () => {
+            'worklet'
+          },
+        )
       }
 
       // if (wx.vibrateShort) wx.vibrateShort({ type: 'heavy' })
@@ -80,7 +131,18 @@ Component({
 
       this.data._bg_color.value = this.data.bgColor
       if (this.data.iconActive) {
-        this.data._btn_opacity.value = 1
+        this.data._btn_opacity.value = timing(
+          1,
+          {
+            duration: 150,
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            easing: Easing.ease,
+          },
+          () => {
+            'worklet'
+          },
+        )
       }
     },
   },
