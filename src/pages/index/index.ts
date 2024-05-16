e437c9c9b0438b23a9dbfdcfd65863250e996cb2
KEVIN YANG <yangws2@midea.com>
2024-05-16 14:02:29 +0800

    fix: 优化弹出菜单，修正【创建】标志及选中图标

--- src/pages/index/index.ts
+++ src/pages/index/index.ts
@@ -179,7 +179,7 @@ ComponentWithComputed({
           key: home.houseId,
           name: home.houseName?.length > 6 ? home.houseName.slice(0, 6) + '...' : home.houseName,
           checked: home.houseId === data.currentHomeId,
-          tag: home.defaultHouseFlag ? '创建' : '',
+          tag: home.houseCreatorFlag ? '创建' : '',
         }))
 
       return list
