16f72642a26ba1cb09fd648c17fa87cd8e5c8542
夏钰明 <xiaym18@midea.com>
2024-05-23 09:30:19 +0800

    feat: 场景创建成功页面改造skyline

--- src/package-automation/scene-success/index.ts
+++ src/package-automation/scene-success/index.ts
@@ -1,17 +1,5 @@
-import { storage } from '../../utils/index'
-
 // package-automation/scene-success/index.ts
 Component({
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    navigationBarAndStatusBarHeight:
-      (storage.get<number>('statusBarHeight') as number) +
-      (storage.get<number>('navigationBarHeight') as number) +
-      'px',
-  },
-
   methods: {
     go2SceneIndex() {
       wx.navigateBack({
