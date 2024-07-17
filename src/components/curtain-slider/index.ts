1eb74050c7fcd82108af11b337095640652f5a63
KEVIN YANG <yangws2@midea.com>
2024-07-17 17:41:03 +0800

    refactor: 窗帘组件，修正模拟器下卡死的问题；change事件不必发送指令，以免过于频繁调用接口；

--- src/components/curtain-slider/index.ts
+++ src/components/curtain-slider/index.ts
@@ -1,29 +1,20 @@
-import { runOnJS, shared, timing } from '../../skyline-components/common/worklet'
+import { runOnJS, shared, timing, GestureState } from '../../skyline-components/common/worklet'
+import { throttle } from '../../utils/index'
 import { storage } from '../../utils/storage'
 
 Component({
+  options: {
+    pureDataPattern: /^_/,
+  },
   properties: {
-    // disabled: Boolean,
-    // max: {
-    //   type: Number,
-    //   value: 100,
-    // },
-    // min: {
-    //   type: Number,
-    //   value: 0,
-    // },
-    // step: {
-    //   type: Number,
-    //   value: 1,
-    // },
     value: {
       type: Number,
       value: 0,
       observer(newVal) {
         //HACK: 直接赋值会导致控制前动画不到位
-        if (this.data.isHandling.value) return
-        this.data.translateX.value = timing(
-          (newVal / 100) * this.data.maxTranslateX.value,
+        if (this.data._isHandling.value) return
+        this.data._translateX.value = timing(
+          Math.round((newVal / 100) * this.data._maxTranslateX),
           {
             duration: 30,
           },
@@ -39,32 +30,30 @@ Component({
    * 组件的初始数据
    */
   data: {
-    windowWidth: { value: 0 },
-    isHandling: { value: false },
-    translateX: { value: 0 },
-    divideRpxByPx: storage.get('divideRpxByPx'),
-    isTouchRight: { value: false },
-    maxTranslateX: { value: 280 },
-    throttleTimer: 0,
+    _windowWidth: wx.getSystemInfoSync().windowWidth,
+    _isHandling: { value: false },
+    _translateX: { value: 0 },
+    _divideRpxByPx: storage.get('divideRpxByPx'),
+    _isTouchRight: { value: false },
+    _maxTranslateX: 280,
   },
   lifetimes: {
     attached() {
-      const { windowWidth } = wx.getSystemInfoSync()
-      this.data.windowWidth = shared(windowWidth)
-      this.data.isTouchRight = shared(false)
-      this.data.isHandling = shared(false)
-      this.data.maxTranslateX = shared(252 * this.data.divideRpxByPx)
-      this.data.translateX = shared((this.data.value / 100) * this.data.maxTranslateX.value)
+      this.data._isTouchRight = shared(false)
+      this.data._isHandling = shared(false)
+      this.data._maxTranslateX = 252 * this.data._divideRpxByPx
+      this.data._translateX = shared((this.data.value / 100) * this.data._maxTranslateX)
+
       this.applyAnimatedStyle('#right-curtain', () => {
         'worklet'
         return {
-          transform: `translateX(${this.data.translateX.value}px)`,
+          transform: `translateX(${this.data._translateX.value}px)`,
         }
       })
       this.applyAnimatedStyle('#left-curtain', () => {
         'worklet'
         return {
-          transform: `translateX(-${this.data.translateX.value}px)`,
+          transform: `translateX(-${this.data._translateX.value}px)`,
         }
       })
     },
@@ -76,52 +65,54 @@ Component({
     drag(evt: { state: number; absoluteX: number; deltaX: number }) {
       'worklet'
       const { state, absoluteX, deltaX } = evt
-      if (state === 3 || state === 4) {
-        runOnJS(this.dragEnd.bind(this))()
-      }
-      if (state === 1) {
-        this.data.isHandling.value = true
-        this.data.isTouchRight.value = absoluteX > this.data.windowWidth.value / 2
-      }
+      // console.log('[drag]', state, absoluteX, deltaX)
 
-      if (state !== 2) return
-      runOnJS(this.valueChange.bind(this))()
-      const nextTranslateX = this.data.isTouchRight.value
-        ? this.data.translateX.value + deltaX
-        : this.data.translateX.value - deltaX
-      if (nextTranslateX <= 0) {
-        this.data.translateX.value = 0
-      } else if (nextTranslateX >= this.data.maxTranslateX.value) {
-        this.data.translateX.value = this.data.maxTranslateX.value
-      } else {
-        this.data.translateX.value = nextTranslateX
+      switch (state) {
+        case GestureState.BEGIN:
+          this.data._isHandling.value = true
+          this.data._isTouchRight.value = absoluteX > this.data._windowWidth / 2
+          break
+
+        case GestureState.END:
+        case GestureState.CANCELLED:
+          runOnJS(this.dragEnd.bind(this))()
+          break
+
+        case GestureState.ACTIVE: {
+          const nextTranslateX = this.data._isTouchRight.value
+            ? this.data._translateX.value + deltaX
+            : this.data._translateX.value - deltaX
+          const posX = Math.min(this.data._maxTranslateX, Math.max(0, nextTranslateX))
+
+          this.data._translateX.value = posX
+
+          runOnJS(this.valueChangeThrottle.bind(this))(posX)
+        }
       }
     },
-    valueChange() {
-      if (this.data.throttleTimer) return
-      const value = Math.round((this.data.translateX.value / this.data.maxTranslateX.value) * 100)
-      this.data.throttleTimer = setTimeout(() => {
-        this.triggerEvent('change', value)
-        this.setData({
-          value,
-        })
-        this.data.throttleTimer = 0
-      }, 150)
+    valueChange(posX: number) {
+      const value = this.xToV(posX)
+      this.triggerEvent('change', value)
+      this.setData({
+        value,
+      })
     },
+    valueChangeThrottle: throttle(function (this: IAnyObject, posX: number) {
+      this.valueChange(posX)
+    }, 300),
     dragEnd() {
-      if (this.data.throttleTimer) {
-        clearTimeout(this.data.throttleTimer)
-        this.data.throttleTimer = 0
-      }
-      const value = Math.round((this.data.translateX.value / this.data.maxTranslateX.value) * 100)
+      const value = this.xToV(this.data._translateX.value)
       this.setData({
         value,
       })
       this.triggerEvent('slideEnd', value)
       //释放标志，允许通过外部value重新计算slider-bar宽度
       setTimeout(() => {
-        this.data.isHandling.value = false
+        this.data._isHandling.value = false
       }, 200)
+    },
+    xToV(posX: number) {
+      return Math.round((posX / this.data._maxTranslateX) * 100)
     },
   },
 })
