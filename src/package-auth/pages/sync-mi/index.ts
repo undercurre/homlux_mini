91053e46ed121accd6c93bdf2931bd7210d8a8b6
KEVIN YANG <yangws2@midea.com>
2024-09-13 16:54:59 +0800

    feat: 小米同步页，菜单选择效果

--- src/package-auth/pages/sync-mi/index.ts
+++ src/package-auth/pages/sync-mi/index.ts
@@ -1,6 +1,6 @@
 import { ComponentWithComputed } from 'miniprogram-computed'
 import pageBehaviors from '../../../behaviors/pageBehaviors'
-import { delay, Logger } from '../../../utils/index'
+import { delay, Logger, storage } from '../../../utils/index'
 import { getToken } from '../../../apis/index'
 import { deviceStore } from '../../../store/index'
 import { SCREEN_PID } from '../../../config/index'
@@ -15,6 +15,30 @@ ComponentWithComputed({
     listHeight: 0,
     loading: false,
     checkIndex: 0, // 选择的家庭index
+    miHomeMenu: {
+      x: '30rpx',
+      y: (storage.get('statusBarHeight') as number) + (storage.get('navigationBarHeight') as number) + 160 + 'px',
+      arrowX: 100,
+      width: 400,
+      height: 300,
+      isShow: false,
+    },
+    miHomeList: [
+      {
+        name: '2483的家',
+        checked: true,
+        tag: '创建',
+      },
+      {
+        name: 'xxxx的家',
+      },
+      {
+        name: 'yyyy的家',
+      },
+      {
+        name: 'zzzz的家',
+      },
+    ],
   },
 
   computed: {},
@@ -49,6 +73,23 @@ ComponentWithComputed({
             })
           }
         })
+    },
+
+    onCheckHome() {
+      this.setData({
+        'miHomeMenu.isShow': true,
+      })
+    },
+
+    handleMenuTap() {
+      this.setData({
+        'miHomeMenu.isShow': false,
+      })
+    },
+    handleMenuClose() {
+      this.setData({
+        'miHomeMenu.isShow': false,
+      })
     },
 
     async toConfirm() {},
