fae3d0019053b2ae2da4828abfc3a82c1f70381b
liumn <mannan.liu@midea.com>
2024-09-24 10:26:52 +0800

    fix: 修复类型错误

--- src/pages/remoter/index.ts
+++ src/pages/remoter/index.ts
@@ -42,7 +42,7 @@ ComponentWithComputed({
     canShowNotFound: false, // 已搜索过至少一次但未找到
     foundList: [] as Remoter.DeviceItem[], // 搜索到的设备
     _bleServer: null as WechatMiniprogram.BLEPeripheralServer | null,
-    _time_id_end: 0, // 定时终止搜索设备
+    _time_id_end: 0 as any, // 定时终止搜索设备
     _lastPowerKey: '', // 记录上一次点击‘照明’时的指令键，用于反转处理
     _timer: 0, // 记录上次指令时间
     _holdBleScan: false, // onHide时保持蓝牙扫描的标志
